cb9ead298678ba661d68c03d11c705f3180948f9
vasilis <vilia002@ucy.ac.cy>
2024-12-07 00:26:17 +0200

    Added weapon to character and StringRepo

---
+++ Assets/StringRepo.cs
@@ -0,0 +1,8 @@
+public static class StringRepo
+{
+    public const string JumpButton = "Jump";
+    public const string HorizontalInput = "Horizontal";
+    public const string VerticalInput = "Vertical";
+    public const string mouseX = "Mouse X";
+    public const string mouseY = "Mouse Y";
+}
--- Assets/Used/Scripts/Player/MouseLook.cs
+++ Assets/Used/Scripts/Player/MouseLook.cs
@@ -5,24 +5,23 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
-    public Transform playerBody; // We link this object to the FIrstPersonPlayer object, therefore camera also transforms
+    public Transform playerBody; // We link this object to the FirstPersonPlayer object, therefore camera also transforms
     float xRotation = 0f;
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked; // To lock cursor in game window
-        
+        Cursor.lockState = CursorLockMode.Locked; // To lock cursor in game window        
     }
  
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis(StringRepo.mouseX) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(StringRepo.mouseY) * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // So the camera does not flip vertically
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // So the camera does not flip vertically    
 
-        playerBody.Rotate(Vector3.up * mouseX);
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(0f, mouseX, 0f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);      
     }
 }
