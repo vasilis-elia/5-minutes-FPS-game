22661b0f66f13b9c58876b83cc98dc36564c6ea2
vasilis <vilia002@ucy.ac.cy>
2024-12-10 21:11:38 +0200

    Extra fixes

--- Assets/Used/Scripts/Monster/RakeMovement.cs
+++ Assets/Used/Scripts/Monster/RakeMovement.cs
@@ -1,10 +1,6 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.Animations;
+
 
 public class RakeMovement : MonoBehaviour
 {
